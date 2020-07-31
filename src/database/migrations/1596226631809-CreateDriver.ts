9a2c1a65eed30130ab6408c5bdceccee00144902
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-31 17:20:56 -0300

    initial commit 🚀🎉

---
+++ src/database/migrations/1596226631809-CreateDriver.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateDriver1596226631809 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
